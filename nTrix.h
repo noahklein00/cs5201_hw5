2607ede27dc8a42447439e4e691b08c0e4be51e8
Noah <nekgr8@mst.edu>
2020-03-22 21:46:24 -0500

    updated matrix code

--- nTrix.h
+++ nTrix.h
@@ -5,10 +5,10 @@
  * Class: CS5201 \n
  * Assignment: Homework 5 \n
  */
- 
+
 #ifndef NTRIX_H
-#define NTRIX_H 
- 
+#define NTRIX_H
+
 #include <iostream>
 #include "nVect.h"
 
@@ -19,49 +19,51 @@ class nTrix
 		unsigned short m_row;
 		unsigned short m_col;
 		T** m_matrix;
-		
+
 	public:
-	
+
 		// Essential //
-		
+
 		nTrix();
 		nTrix(const std::initializer_list<std::initializer_list<T>> grid);
 		nTrix(const short num_rows, const short num_cols);
+    nTrix(const nVect<T>& rhs);
 		nTrix(const nTrix<T>& rhs);
 		nTrix<T>& operator=(const nTrix<T>& rhs);
 		~nTrix();
-		
+
 		// Accessors //
-		
+
 		unsigned short rows() const;
 		unsigned short cols() const;
-		
+
 		// Mutators //
-		
+
 		T& operator()(const int row_index, const int col_index);
-	
+    nTrix<T> operator+(const nTrix<T>& rhs) const;
+    nTrix<T> operator-(const nTrix<T>& rhs) const;
+    nTrix<T> operator-() const;
+    nTrix<T> operator*(const nTrix<T>& rhs) const;
+    nTrix<T> operator*(const nVect<T>& rhs) const;
+
 		// Friend //
 		template <typename U>
 		friend std::ostream& operator<<(std::ostream& out, const nTrix<U>& rhs);
-		
-		template <typename U>
-		friend nTrix<U> operator+(const nTrix<U>& lhs, const nTrix<U>& rhs);
-		
-		template <typename U>
-		friend nTrix<U> operator*(const nTrix<U>& lhs, const nTrix<U>& rhs);
+
+    // template <typename U>
+    // friend nTrix<U> operator*(const nTrix<U>& lhs, const float scalar);
 };
+
+// Extras //
 
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const nTrix<T>& rhs);
 
 template <typename T>
-nTrix<T> operator+(const nTrix<T>& lhs, const nTrix<T>& rhs);
+nTrix<T> operator*(const nTrix<T>& lhs, const float scalar);
 
 template <typename T>
-nTrix<T> operator-(const nTrix<T>& lhs, const nTrix<T>& rhs);
-
-template <typename T>
-nTrix<T> operator*(const nTrix<T>& lhs, const nTrix<T>& rhs);
+nTrix<T> operator*(const nVect<T>& lhs, const nTrix<T>& rhs);
 
 #include "nTrix.hpp"
 #endif
--- nTrix.hpp
+++ nTrix.hpp
@@ -1,7 +1,7 @@
 // Programmer : Noah Klein
 // Instructor : Price
 // Class      : CS5201 Spring 2020
-// Assignment : Homework 4 - Implicit Solvers, p-norms, and 
+// Assignment : Homework 4 - Implicit Solvers, p-norms, and
 //					Insulin/Glucose Dynamics
 // Filename   : nVect.hpp
 
@@ -14,7 +14,7 @@ nTrix<T>::nTrix()
 	m_row = 2;
 	m_col = 2;
 	m_matrix = new T*[m_row];
-	
+
 	for(int i = 0; i < m_row; i++)
 	{
 		m_matrix[i] = new T[m_col];
@@ -27,19 +27,19 @@ nTrix<T>::nTrix(const std::initializer_list<std::initializer_list<T>> grid)
 	m_row = grid.size();
 	m_col = grid.begin() -> size();
 	m_matrix = new T*[m_row];
-	
+
 	unsigned short row_counter = 0;
 	unsigned short col_counter = 0;
-	
+
 	for(auto r_itr = grid.begin(); r_itr != grid.end(); r_itr++)
 	{
-		if(r_itr -> size() != m_col) 
+		if(r_itr -> size() != m_col)
 		{
 			throw(std::domain_error(std::to_string(r_itr -> size())));
 		}
-		
+
 		m_matrix[row_counter] = new T[m_col];
-		
+
 		for(auto c_itr = r_itr -> begin(); c_itr != r_itr -> end(); c_itr++)
 		{
 			m_matrix[row_counter][col_counter] = *c_itr;
@@ -61,16 +61,30 @@ nTrix<T>::nTrix(const short num_rows, const short num_cols)
 	{
 		throw(std::domain_error(std::to_string(num_cols)));
 	}
-	
+
 	m_row = num_rows;
 	m_col = num_cols;
 	m_matrix = new T*[m_row];
-	
+
 	for(int i = 0; i < m_row; i++)
 	{
 		m_matrix[i] = new T[m_col];
 	}
-}	
+}
+
+template <typename T>
+nTrix<T>::nTrix(const nVect<T>& rhs)
+{
+	m_row = rhs.size();
+	m_col = 1;
+	m_matrix = new T*[m_row];
+
+	for(int i = 0; i < m_row; i++)
+	{
+		m_matrix[i] = new T[m_col];
+		m_matrix[i][0] = rhs[i];
+	}
+}
 
 template <typename T>
 nTrix<T>::nTrix(const nTrix<T>& rhs)
@@ -78,7 +92,7 @@ nTrix<T>::nTrix(const nTrix<T>& rhs)
 	m_row = rhs.m_row;
 	m_col = rhs.m_col;
 	m_matrix = new T*[m_row];
-	
+
 	for(int i = 0; i < m_row; i++)
 	{
 		m_matrix[i] = new T[m_col];
@@ -87,7 +101,7 @@ nTrix<T>::nTrix(const nTrix<T>& rhs)
 			m_matrix[i][j] = rhs.m_matrix[i][j];
 		}
 	}
-}	
+}
 
 template <typename T>
 nTrix<T>& nTrix<T>::operator=(const nTrix<T>& rhs)
@@ -99,11 +113,11 @@ nTrix<T>& nTrix<T>::operator=(const nTrix<T>& rhs)
 	}
 	delete[] m_matrix;
 	m_matrix = NULL;
-	
+
 	m_row = rhs.m_row;
 	m_col = rhs.m_col;
 	m_matrix = new T*[m_row];
-	
+
 	for(int i = 0; i < m_row; i++)
 	{
 		m_matrix[i] = new T[m_col];
@@ -112,9 +126,9 @@ nTrix<T>& nTrix<T>::operator=(const nTrix<T>& rhs)
 			m_matrix[i][j] = rhs.m_matrix[i][j];
 		}
 	}
-	
+
 	return *this;
-}		
+}
 
 template <typename T>
 nTrix<T>::~nTrix()
@@ -147,7 +161,7 @@ unsigned short nTrix<T>::cols() const
 template <typename T>
 T& nTrix<T>::operator()(const int row_index, const int col_index)
 {
-	if(row_index < 0 || row_index >= m_row) 
+	if(row_index < 0 || row_index >= m_row)
 	{
 		throw(std::domain_error(std::to_string(row_index)));
 	}
@@ -155,15 +169,92 @@ T& nTrix<T>::operator()(const int row_index, const int col_index)
 	{
 		throw(std::domain_error(std::to_string(col_index)));
 	}
-	
+
 	return m_matrix[row_index][col_index];
 }
 
+template <typename T>
+nTrix<T> nTrix<T>::operator+(const nTrix<T>& rhs) const
+{
+	if(this -> rows() != rhs.rows())
+	{
+		throw(std::invalid_argument(std::to_string(this -> rows())));
+	}
+	if(this -> cols() != rhs.cols())
+	{
+		throw(std::invalid_argument(std::to_string(this -> cols())));
+	}
+
+	nTrix<T> copy(*this);
+
+	for(int i = 0; i < copy.m_row; i++)
+	{
+		for(int j = 0; j < copy.m_col; j++)
+		{
+			copy.m_matrix[i][j] += rhs.m_matrix[i][j];
+		}
+	}
+
+	return copy;
+}
+
+template <typename T>
+nTrix<T> nTrix<T>::operator-(const nTrix<T>& rhs) const
+{
+	return *this + (-rhs);
+}
+
+template <typename T>
+nTrix<T> nTrix<T>::operator-() const
+{
+	return (*this*(-1));
+}
+
+template <typename T>
+nTrix<T> nTrix<T>::operator*(const nTrix<T>& rhs) const
+{
+	if(this -> cols() != rhs.rows())
+	{
+		throw(std::invalid_argument(std::to_string(this -> cols())));
+	}
+
+	short sum = 0;
+	short left_row = 0;
+	short right_col = 0;
+
+	nTrix<T> copy(this -> rows(),rhs.cols());
+
+	for(int i = 0; i < copy.rows(); i++)
+	{
+		for(int j = 0; j < copy.cols(); j++)
+		{
+			for(int k = 0; k < this -> cols(); k++)
+			{
+				sum += (this -> m_matrix[left_row][k] * rhs.m_matrix[k][right_col]);
+			}
+			copy(i,j) = sum;
+			sum = 0;
+			right_col++;
+		}
+		right_col = 0;
+		left_row++;
+	}
+
+	return copy;
+}
+
+template <typename T>
+nTrix<T> nTrix<T>::operator*(const nVect<T>& rhs) const
+{
+	nTrix<T> copy(rhs);
+	return *this * copy;
+}
+
 //***************************** Friends *************************************//
-	
+
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const nTrix<T>& rhs)
-{	
+{
 	for(int i = 0; i < rhs.m_row; i++)
 	{
 		for(int j = 0; j < rhs.m_col; j++)
@@ -178,75 +269,31 @@ std::ostream& operator<<(std::ostream& out, const nTrix<T>& rhs)
 	return out;
 }
 
-template <typename T>
-nTrix<T> operator+(const nTrix<T>& lhs, const nTrix<T>& rhs)
-{
-	if(lhs.rows() != rhs.rows())
-	{
-		throw(std::invalid_argument(std::to_string(lhs.rows())));
-	}
-	if(lhs.cols() != rhs.cols())
-	{
-		throw(std::invalid_argument(std::to_string(lhs.cols())));
-	}
-	
-	nTrix<T> copy(lhs);
-	
-	for(int i = 0; i < copy.m_row; i++)
-	{
-		for(int j = 0; j < copy.m_col; j++)
-		{
-			copy.m_matrix[i][j] += rhs.m_matrix[i][j];
-		}
-	}
-	
-	return copy;
-}
+//***************************** Extras **************************************//
 
 template <typename T>
-nTrix<T> operator*(const nTrix<T>& lhs, const nTrix<T>& rhs)
+nTrix<T> operator*(const nTrix<T>& lhs, const float scalar)
 {
-	if(lhs.cols() != rhs.rows())
-	{
-		throw(std::invalid_argument(std::to_string(lhs.cols())));
-	}
-	
-	short sum = 0;
-	short left_row = 0;
-	short right_col = 0;
-	
-	nTrix<T> copy(lhs.rows(),rhs.cols());
-	
+	nTrix<T> copy(lhs);
 	for(int i = 0; i < copy.rows(); i++)
 	{
 		for(int j = 0; j < copy.cols(); j++)
 		{
-			for(int k = 0; k < lhs.cols(); k++)
-			{
-				sum += (lhs.m_matrix[left_row][k] * rhs.m_matrix[k][right_col]);
-			}
-			copy(i,j) = sum;
-			sum = 0;
-			right_col++;
+			copy(i,j) *= scalar;
 		}
-		right_col = 0;
-		left_row++;
 	}
-	
 	return copy;
 }
 
+template <typename T>
+nTrix<T> operator*(const float scalar, const nTrix<T>& rhs)
+{
+	return (rhs*scalar);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+template <typename T>
+nTrix<T> operator*(const nVect<T>& lhs, const nTrix<T>& rhs)
+{
+	nTrix<T> copy(lhs);
+	return copy * rhs;
+}
